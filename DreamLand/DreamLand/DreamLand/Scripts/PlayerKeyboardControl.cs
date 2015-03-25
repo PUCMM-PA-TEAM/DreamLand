460a7c55bd07b8b4fa444a957b0a0916379b458b
Max Medina <maxmedina05@gmail.com>
2015-03-24 23:38:14 -0400

    Add Visual Solution
    This contains all the visual studios files and also i added the sprites
    that are going to be used in the game.

---
+++ DreamLand/DreamLand/DreamLand/Scripts/PlayerKeyboardControl.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Xna.Framework.Input;
+using DreamLand.Scripts.classes;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace DreamLand.Scripts {
+    class PlayerKeyboardControl : XNAScript
+    {
+        private KeyboardState currentKeyboardState;
+        private KeyboardState previousKeyboardState;
+
+        public void Init()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DreamLand/DreamLand/DreamLand/Scripts/classes/XNAScript.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace DreamLand.Scripts.classes {
+    interface XNAScript
+    {
+        void Init();
+        void Update(GameTime gameTime);
+        void Draw(SpriteBatch spriteBatch);
+
+    }
+}
