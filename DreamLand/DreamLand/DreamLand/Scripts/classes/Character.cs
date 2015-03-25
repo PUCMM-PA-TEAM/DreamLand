b45aa5f29bd9fe4aead9325b39fbccda4877af09
Max Medina <maxmedina05@gmail.com>
2015-03-25 00:03:04 -0400

    Add scripts

---
+++ DreamLand/DreamLand/DreamLand/Scripts/classes/Character.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DreamLand.Scripts.classes {
+    abstract class Character
+    {
+        public String Name { get; set; }
+        public int Health { get; set; }
+        public int Strength { get; set; }
+        public int Magic { get; set; }
+        public int Defense { get; set; }
+        public int Speed { get; set; }
+        public int Level { get; set; }
+        public int Damage { get; set; }
+
+
+    }
+}
